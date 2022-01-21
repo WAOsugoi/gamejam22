04abc48fdbac362f6505b9f96a5028bae2ca8bde
Jeromesyl <jsyl3214@gmail.com>
2022-01-22 01:59:58 +0800

    Update spawner script

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,16 +4,10 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject spawnObject;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Instantiate(spawnObject, transform.position, transform.rotation);
-    }
+    public GameObject objectToBeSpawned;
 
-    // Update is called once per frame
-    void Update()
+    public void spawnObject()
     {
-
+        Instantiate(objectToBeSpawned, transform.position, transform.rotation);
     }
 }
