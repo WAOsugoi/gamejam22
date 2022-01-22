ad5bfabdc9f87b9431f44c6120375f5ebfcf891e
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2022-01-23 03:28:06 +0800

    cat sprite changer

--- Assets/CatSpriteManager.cs
+++ Assets/CatSpriteManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CatSpriteManager : MonoBehaviour
 {
+    
     public Sprite[] Latte = new Sprite[5];
     public Sprite[] Vanila = new Sprite[5];
     public Sprite[] Matcha = new Sprite[5];
 
     Cat_Behaviour cat;
+    SpriteRenderer catSprite;
+    Sprite[] selectedSprite;
     enum CAT_TYPE
     {
         LATTE,
@@ -23,11 +26,25 @@ public class CatSpriteManager : MonoBehaviour
     {
         currCatType = (CAT_TYPE)Random.Range(0, 3);
         cat = gameObject.GetComponentInParent<Cat_Behaviour>();
+        catSprite = gameObject.GetComponentInParent<SpriteRenderer>();
+        switch(currCatType)
+        {
+            case CAT_TYPE.LATTE:
+                selectedSprite = Latte;
+                break;
+            case CAT_TYPE.VANILLA:
+                selectedSprite = Vanila;
+                break;
+            case CAT_TYPE.MATCHA:
+                selectedSprite = Matcha;
+                break;
+        }
+        catSprite.sprite = selectedSprite[2];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        catSprite.sprite = selectedSprite[(int)cat.temperature_state_flag];
     }
 }
