19310a1fd7b4329c7dbdb0c6be9fe7021e1ec071
dennysTayKT <90560835+dennysTayKT@users.noreply.github.com>
2022-01-22 10:53:22 +0800

    Updated Cat Logic
    Added functionality to Cat Sin Generator/Cat_Behaviour class.
    
    Now we can make a list of cat sins
    and a condition for triggering win/losing

--- Assets/Scripts/Cat_Behaviour.cs
+++ Assets/Scripts/Cat_Behaviour.cs
@@ -4,33 +4,67 @@ using UnityEngine;
 
 //CatSinGenerator catsingen = GetComponent<CatSinGenerator>();
 
+
 public class Cat_Behaviour : MonoBehaviour
 {
-    double temperature;
-    enum temp_state {frozen, cold, neutral, hot, boiling};
 
-    
-    //I'm using a coroutine to be sure the list in Script1 is created before I print the items in the list.
-     IEnumerator WaitForSinList()
-     {
-         //catsingen = GetComponent<CatSinGenerator>();
-         yield return new WaitForEndOfFrame();
+    enum temp_state {frozen, cold, neutral, hot, boiling}
 
+    public bool isBad = false;
 
-         //foreach(string s in s1.testList)
-     }  
+    public double temperature;
+    temp_state temperature_state_flag;
+
+    CatSinGenerator catsingen;
+    public GameObject logicmanager;
 
 
+
+    void Awake()
+    {
+        catsingen = GetComponent<CatSinGenerator>();
+        logicmanager = GameObject.Find("GameLogicManager");
+        
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         
+        temperature = (double)Random.Range(30, 70);
+        RefreshTemperature();
+    
+        //checks if cat is good or bad internally        
+        if (catsingen.goodDisposition < catsingen.badDisposition)
+        {
+            isBad = true;
+        }
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        RefreshTemperature();
     }
+
+    void RefreshTemperature()
+    {
+        //checks for cat current temperature
+        if (temperature == 0.0) {temperature_state_flag = temp_state.frozen;}
+        if ((temperature > 0.0) && (temperature <= 33.3)) {temperature_state_flag = temp_state.cold;}
+        if ((temperature > 33.3) && (temperature <= 66.6)) {temperature_state_flag = temp_state.neutral;}
+        if ((temperature > 66.6) && (temperature <= 99.9)) {temperature_state_flag = temp_state.hot;}
+        else {temperature_state_flag = temp_state.boiling;}
+
+        if ((temperature_state_flag == temp_state.boiling) || (temperature_state_flag == temp_state.frozen))
+        {
+           if ((temperature_state_flag == temp_state.boiling) && (isBad == true)) {logicmanager.UpdateNumWrongJudgement();}
+           else if ((temperature_state_flag == temp_state.frozen) && (isBad == false)) {logicmanager.UpdateNumWrongJudgement();}
+           else {logicmanager.UpdateNumCorrectJudgement();}
+        }
+    }
+
+
+
 }
